56922961716e2676aca78b6dc551d9def5dba8d0
Vasanta2002 <112274976+Vasanta2002@users.noreply.github.com>
2023-03-29 13:55:30 -0400

    uses functions to do the same thing

--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,11 +1,26 @@
 import sqlite3
 import queries as q
 
-connection = sqlite3.connect('rpg_db.sqlite3')
 
-cursor = connection.cursor()
+# DB connect function
+# 1 connection = sqlite3.connect('rpg_db.sqlite3')
+# in function form
+def connect_to_db(db_mane='rpg_db.sqlite3'):
+    return sqlite3.connect(db_mane)
 
-results = cursor.execute(q.SELECT_ALL).fetchall()
+
+# The steps bellow in a function
+# 2 cursor = connection.cursor()
+# 3 results = cursor.execute(q.SELECT_ALL).fetchall()
+def execute_q(conn, query):
+    # make the "cursor"
+    curs = conn.cursor()
+    # execute the query
+    curs.execute(query)
+    # pull the results and return
+    return curs.fetchall()
+
 
 if __name__ == '__main__':
-    print(results[:5])
+    conn = connect_to_db()
+    print(execute_q(conn, q.SELECT_ALL))
